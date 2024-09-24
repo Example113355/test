57dc418ca062ee45a7debccef5d9739c31420bbb
TranSon <tson113355@gmail.com>
2024-09-24 09:41:25 +0700

    Refactor build script to remove unnecessary collectstatic command

--- build.sh
+++ build.sh
@@ -4,7 +4,6 @@ python3 -m pip install -r requirements.txt
 python3 manage.py makemigrations
 python3 manage.py migrate
 
-python3 manage.py collectstatic --noinput
 python3 manage.py runserver
 
 python3 manage.py process_tasks
