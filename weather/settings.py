afdb5b40d4c3d1596ff705259d9c99cc7ad65c6d
TranSon <tson113355@gmail.com>
2024-09-24 09:39:31 +0700

    Update static file settings

--- weather/settings.py
+++ weather/settings.py
@@ -145,7 +145,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR/'staticfiles',]
 STATIC_ROOT = BASE_DIR/'staticfiles'
 
 # Default primary key field type
